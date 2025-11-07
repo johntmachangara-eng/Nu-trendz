519f131af9308cc3ee5316cb3a0315c64e812595
johntmachangara-eng <johntmachangara@gmail.com>
2025-11-07 20:29:50 +0000

    Add files via upload

---
+++ shop.js
@@ -0,0 +1,29 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+const totalSlides = slides.length;
+
+document.querySelector(".next").addEventListener("click", nextSlide);
+document.querySelector(".prev").addEventListener("click", prevSlide);
+
+function showSlide(index) {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slides[index].classList.add("active");
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % totalSlides;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+  showSlide(currentSlide);
+}
+
+// Auto-slide every 6 seconds
+setInterval(nextSlide, 6000);
+
+const video = document.querySelector('.portrait-video');
+video.addEventListener('loadeddata', () => {
+  video.classList.add('loaded');
+});
